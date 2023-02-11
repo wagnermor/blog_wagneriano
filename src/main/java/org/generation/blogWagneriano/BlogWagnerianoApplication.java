1967013235b04b65635c12a5b1d2292f5a61be95
WagnerMor <wagnermor@outlook.com>
2023-02-11 12:53:53 -0300

    New: start spring boot project

---
+++ src/main/java/org/generation/blogWagneriano/BlogWagnerianoApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogWagneriano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWagnerianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWagnerianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/generation/blogWagneriano/BlogWagnerianoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogWagneriano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWagnerianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
