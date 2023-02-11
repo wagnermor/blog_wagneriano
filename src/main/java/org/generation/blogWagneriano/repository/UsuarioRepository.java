877be78859d0bf09c132166e0de438aab54e7dd7
WagnerMor <wagnermor@outlook.com>
2023-02-11 19:16:58 -0300

    Fix: add Query Method in /repository/UsuarioRepository.java

--- src/main/java/org/generation/blogWagneriano/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogWagneriano/repository/UsuarioRepository.java
@@ -1,14 +1,16 @@
 package org.generation.blogWagneriano.repository;
 
+import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import org.generation.blogWagneriano.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	public Optional<Usuario> findByUsuario(String usuario);	
+	public Optional<Usuario> findByUsuario(String usuario);
+	public List <Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
