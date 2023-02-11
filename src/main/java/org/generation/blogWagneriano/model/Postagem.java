a3a290160f71a619879bc8d2a3769d2889efced0
WagnerMor <wagnermor@outlook.com>
2023-02-11 16:31:28 -0300

    Fix: Add getUsuario and setUsuario methods in /model/Postagem.java

--- src/main/java/org/generation/blogWagneriano/model/Postagem.java
+++ src/main/java/org/generation/blogWagneriano/model/Postagem.java
@@ -83,4 +83,12 @@ public class Postagem {
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
