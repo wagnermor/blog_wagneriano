eda628704cc47c06bbf4bc3dd3541f1c4be9cf48
WagnerMor <wagnermor@outlook.com>
2023-02-11 13:32:39 -0300

    Fix: add query findAllByTitulo /repository/PostagemRepository.java

--- src/main/java/org/generation/blogWagneriano/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogWagneriano/repository/PostagemRepository.java
@@ -1,10 +1,13 @@
 package org.generation.blogWagneriano.repository;
 
+import java.util.List;
+
 import org.generation.blogWagneriano.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 }
